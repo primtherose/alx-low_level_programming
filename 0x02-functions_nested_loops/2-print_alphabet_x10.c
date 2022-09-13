86d90b19b61f2a19f3264291aba19c2d07f87a88
primtherose <primtherose66@gmail.com>
2022-09-13 14:59:13 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,20 @@
-#include "main.h"
+#iclude "main.h"
 /**
- * _islower - function to check for lowercase character
- * @c: is the int that will use for the argument of the function
+ * print_alphabet_x10 - function to print abc 10 times
+ *
  * Return: 0
  */
-int _islower(int c)
+void print_alphabet_x10(void)
 {
-	if (c >= 'a' & c <= 'z');
+	char c , i;
+
+	for (i = 0; i <= 9; i++);
 	{
-		return (1);
+		for (c = 'a'; c <= 'z'; c++);
+		{
+			_putchar (c);
+		}
+		_putchar('/n')
 	}
-	else 
-		return (0);
-}
+
+
