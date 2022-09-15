6650242ab4893a7c147f56661d68e18ce6721d0c
primtherose <primtherose66@gmail.com>
2022-09-15 12:05:19 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,12 @@
-#include "main.h"
+include "main.h"
 
-/**
- * main - Test function for positive or negative
+/*
+ * main - tests function that prints if integer is positive or negative
+ *
  * Return: 0
+ *
  */
+
 int main(void)
 {
 	int i;
