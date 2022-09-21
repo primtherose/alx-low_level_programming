7927ee7a3eec8db13b5ef80263cc38cdb2f53b1f
primtherose <primtherose66@gmail.com>
2022-09-21 07:11:50 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The 1st string
+ * @s2: the 2nd string
+ *
+ * Return: int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
