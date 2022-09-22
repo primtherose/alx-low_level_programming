a584148c12870bcd031db07233e23462242725e5
primtherose <primtherose66@gmail.com>
2022-09-22 12:34:42 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 
 /**
  * _strcat - concatenates the spring pointed to by @src to
- * to the end of the string pointed to by @dest
- * @dest string that will be appended
- * @src string that will be concatenated upon
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String that will be concatenated upon
  *
  * Return: returns pointer to @dest
  */
@@ -18,5 +18,6 @@ char *_strcat(char *dest, char *src)
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
+
 	return (dest);
 }
