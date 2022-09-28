b903161121ec7581c87c4042d442460e36ddfd22
primtherose <primtherose66@gmail.com>
2022-09-28 13:41:08 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: string to be printed
  *
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
