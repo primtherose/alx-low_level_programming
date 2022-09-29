c7f9eecae7210c1a5337ce1e7960300a428348f0
primtherose <primtherose66@gmail.com>
2022-09-29 12:55:06 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - function to check if character is uppercase
  *
- * @c checks input of functions
+ * @c: checks input of functions
  * Return: returns 1 if 'c' is uppercase, otherwise always 0 success
  */
 
