f0e0c1cbc8fee0b2fc027b9802eda8d6a2a18201
primtherose <primtherose66@gmail.com>
2022-10-10 14:14:45 -0700

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_likE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(s) ((x < 0) ? (x) * (-1) : (x))
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 
 #endif
