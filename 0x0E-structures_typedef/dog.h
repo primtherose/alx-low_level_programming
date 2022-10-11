88ae957496b1305ce0da1580ffc948396ade78f4
primtherose <primtherose66@gmail.com>
2022-10-11 12:34:35 -0700

    Task 0-2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,25 +2,20 @@
 #define DOG_H
 
 /**
- * struct dog - Struct dog
+ * struct dog - Structure definition of a dog
  * @name: name
  * @age: age
  * @owner: owner
- * Description: This struct is for dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char age;
 	char *owner;
-};
-/**
- * dog_t -Typedef for struct dog
- */
-typedef struct dog dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /*DOG_H*/
+void free_dog(dog_t *dog);
+#endif
