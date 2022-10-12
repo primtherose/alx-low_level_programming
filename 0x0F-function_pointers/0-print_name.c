8af5204fade97542aa20b13bf3b100a91732231c
primtherose <primtherose66@gmail.com>
2022-10-12 11:14:55 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 /**
  * print_name - prints a name.
  * @name: pointer to name.
- * @f: function pointer
+ * @f: function pointer.
  * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
