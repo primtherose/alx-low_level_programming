43cc052d21ad7a22562c8f3f9a9be9bb8d37d86b
primtherose <primtherose66@gmail.com>
2022-10-31 13:37:46 -0700

    Task o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,11 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, o_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer = NULL)
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
